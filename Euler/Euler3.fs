adc1068aa2cafd953e58de08784a36d096361327
Dave Mateer <davemateer@gmail.com>
2015-10-08 08:21:48 +0100

    working on Euler3 and big ints trying to get it to mod

--- Euler/Euler3.fs
+++ Euler/Euler3.fs
@@ -1,21 +1,30 @@
 ﻿// The prime factors of 13195 are 5, 7, 13 and 29.
 // What is the largest prime factor of the number 600851475143 ?
 module Euler3
-printfn "hello3"
+open Microsoft.FSharp.Math
+open System.Numerics
+
 let Solution1() =
     // get a list of prime numbers...
     // a number is prime if it can only divide by itself and 1
-    let numbers = [1..30]
-    let primes = [1] 
-    for i in numbers do
-//        if i % 2 = 0 then 
-//        else
-            // add to the primes list
-            let newList = i :: primes
-            printfn "%A" newList
-          
+    let numbers = [1..3000]
+    // A generator
+    let primes =
+         [for i in numbers do
+            if i % 2 <> 0 then
+                let divisors = [3..2..i-1]
+                let mutable isPrime = true
+                for j in divisors do
+                    if i % j = 0 then
+                        isPrime <- false
+                if isPrime then
+                    yield i]
 
-    
+    // largest prime factor.. notice I on the end to denote a bigint
+    let bigNum = 600851475143I
+    for i in primes do
+        if bigNum % i = 0 then
+            printfn "%i is a factor" i
 
     // does 13195 / that prime number have 0 remainder.. if yes, then its a prime factor
 Solution1()
