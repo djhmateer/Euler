b5c9d18e10f4f08aef4dd70e3c120b50de07befa
Dave Mateer <davemateer@gmail.com>
2015-10-08 17:06:41 +0100

    got Euler3 working by converting into a bigint

--- Euler/Euler3.fs
+++ Euler/Euler3.fs
@@ -2,12 +2,11 @@
 // What is the largest prime factor of the number 600851475143 ?
 module Euler3
 open Microsoft.FSharp.Math
-open System.Numerics
 
 let Solution1() =
     // get a list of prime numbers...
     // a number is prime if it can only divide by itself and 1
-    let numbers = [1..3000]
+    let numbers = [1..7000]
     // A generator
     let primes =
          [for i in numbers do
@@ -23,9 +22,12 @@ let Solution1() =
     // largest prime factor.. notice I on the end to denote a bigint
     let bigNum = 600851475143I
     for i in primes do
-        if bigNum % i = 0 then
+//        let x = bigint 10
+        let bigInt (x:int) = bigint(x)
+        if bigNum % (i |> bigInt) = 0I then
             printfn "%i is a factor" i
 
     // does 13195 / that prime number have 0 remainder.. if yes, then its a prime factor
+    // 6857
 Solution1()
 
